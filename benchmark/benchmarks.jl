2e553ef726a51890b7c48d3ae9b942de3ba56661
Monssaf Toukal <tmonssaf@gmail.com>
2022-01-20 14:35:21 -0500

    fix finalize in bmark

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -5,11 +5,6 @@ using LinearAlgebra, Logging, Printf, SparseArrays
 # JSO packages
 using Krylov, LinearOperators, NLPModels, NLPModelsModifiers, SolverBenchmark, SolverCore, SolverTools, ADNLPModels, SolverTest, JSOSolvers, CUTEst
 
-function run_cutest_problem(nlp::CUTEstModel)
-  lbfgs(nlp)
-  finalize(nlp)
-end
-
 problem_names = CUTEst.select(;only_free_var=true, max_con=0, min_var=5, max_var=500)
 cutest_problems = ((p, CUTEstModel(p)) for p in problem_names)
 
@@ -17,7 +12,7 @@ const SUITE = BenchmarkGroup()
 SUITE[:cutest_lbfgs] = BenchmarkGroup()
 
 for (p, nlp) ∈ cutest_problems
-  SUITE[:cutest_lbfgs][p] = @benchmarkable run_cutest_problem($nlp)
+  SUITE[:cutest_lbfgs][p] = @benchmarkable lbfgs($nlp)
   finalize(nlp)
 end
 
