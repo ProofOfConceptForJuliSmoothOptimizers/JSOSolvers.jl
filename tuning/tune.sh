4fb5eb64c8152df72f85b049c224084b587e962b
Monssaf Toukal <tmonssaf@gmail.com>
2022-12-27 16:02:12 -0500

    add resolve in bash script

--- tuning/tune.sh
+++ tuning/tune.sh
@@ -4,5 +4,6 @@ export tuningFile="$1"
 export org="$2"
 export repo="$3"
 export pullrequest="$4"
+julia --project=tuning -E 'using Pkg; Pkg.resolve()'
 julia --project=tuning tuning/${tuningFile} &> "$org"_"$repo"_"$pullrequest".txt
 julia --project=tuning tuning/send_gist_url.jl "$org" "$repo" "$pullrequest"
